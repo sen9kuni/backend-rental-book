a6c2f7e6fc4b0b490e7234e0989489ac06439f6b
prayogo Santoso <prayogo.santoso681@gmail.com>
2025-01-02 22:45:56 +0700

    create type for book and rental

--- src/infrastructure/entity/interface.ts
+++ src/infrastructure/entity/interface.ts
@@ -2,10 +2,15 @@
 // ISession
 // INote
 
-import type { Session, User } from "@prisma/client";
+import type { Session, User, Book, Rental } from "@prisma/client";
 
 export type CreateUser = Omit<User, "id">;
 export type UpdateUser = Partial<User>;
+export type CreateBook = Omit<Book, "id">;
+export type UpdateBook = Partial<Book>;
+export type CreateRental = Omit<Rental, "id">;
+export type UpdateRental = Partial<Rental>;
+export type DataBook = Book;
 
 export interface IUser {
 	getAll: () => Promise<User[]>;
@@ -19,4 +24,20 @@ export interface ISession {
 	getOne: (sessionId: string) => Promise<Session | null>;
 	create: (userId: string) => Promise<Session>;
 	delete: (sessionId: string) => Promise<void>;
+}
+
+export interface IBook {
+	getAll: (search?: string) => Promise<Book[]>;
+	getOne: (id: string) => Promise<Book | null>;
+	create: (data: CreateBook) => Promise<Book>;
+	update: (id: string, data: UpdateBook) => Promise<Book>;
+	delete: (id: string) => Promise<void>;
+}
+
+export interface IRental {
+	getAll: (userId: string) => Promise<Rental[]>;
+	getOne: (id: string) => Promise<Rental>;
+	create: (data: CreateRental) => Promise<Rental>;
+	update: (id: string, data: UpdateRental) => Promise<Rental>;
+	delete: (id: string) => Promise<void>;
 }
--- src/infrastructure/entity/types.ts
+++ src/infrastructure/entity/types.ts
@@ -1,6 +1,8 @@
 export const TYPES = {
 	userRepo: Symbol.for("UserRepository"),
 	sessionRepo: Symbol.for("SessionRepository"),
+	bookRepo: Symbol.for("BookRepository"),
+	rentalRepo: Symbol.for("RentalRepository"),
 	prisma: Symbol.for("PrismaClient"),
 	logger: Symbol.for("Logger"),
 };
